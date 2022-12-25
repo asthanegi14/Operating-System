5e9be843671f63d57ffa078e7c949aab6ae8707a
Astha Negi <asthanegi539@gmail.com>
2022-12-25 21:50:54 +0530

    Update round robin.c

--- scheduling algorithms/round robin.c
+++ scheduling algorithms/round robin.c
@@ -1,83 +1,72 @@
-#include<stdio.h>
- 
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
 int main()
 {
-      int i, p, total = 0, x, counter = 0, tq;
-      int wt = 0, tat = 0, at[10], bt[10], temp[10];
-      float avgWt, avgTat;
-      printf("Enter Total Number of Processes: ");
-      scanf("%d", &p);
-      x = p;
-      for(i = 0; i < p; i++)
-      {
-            printf("Arrival Time of process %d: ",i+1);
- 
-            scanf("%d", &at[i]);
- 
-            printf("Burst Time of process %d: ",i+1);
- 
-            scanf("%d", &bt[i]);
- 
-            temp[i] = bt[i];
-      }
- 
-      printf("\nEnter Time Quantum: ");
-      scanf("%d", &tq);
-      
-      for(int i=0;i<p-1;i++){
-          for(int j=0;j<p-i-1;j++){
-              if(at[j+1]<at[j]){
-                  int temp = at[j+1];
-                  at[j+1]=at[j];
-                  at[j]=temp;
-                  
-                  int temp2 = bt[j+1];
-                  bt[j+1]=bt[j];
-                  bt[j]=temp2;
-              }
-          }
-      }
-      
-      printf("\nProcess ID\t\tBurst Time\t Turnaround Time\t Waiting Time\n");
-      
-      for(total = 0, i = 0; x != 0;)
-      {
-            if(temp[i] <= tq && temp[i] > 0)
-            {
-                  total = total + temp[i];
-                  temp[i] = 0;
-                  counter = 1;
-            }
-            else if(temp[i] > 0)
-            {
-                  temp[i] = temp[i] - tq;
-                  total = total + tq;
-            }
-            if(temp[i] == 0 && counter == 1)
-            {
-                  x--;
-                  printf("\nProcess[%d]\t\t%d\t\t %d\t\t\t %d", i + 1, bt[i], total - at[i], total - at[i] - bt[i]);
-                  wt += total - at[i] - bt[i];
-                  tat += total - at[i];
-                  counter = 0;
-            }
-            if(i == p - 1)
-            {
-                  i = 0;
-            }
-            else if(at[i + 1] <= total)
-            {
-                  i++;
-            }
-            else
-            {
-                  i = 0;
-            }
-      }
- 
-      avgWt = wt * 1.0 / p;
-      avgTat = tat * 1.0 / p;
-      printf("\nAverage Waiting Time: %f", avgWt);
-      printf("\nAvg Turnaround Time: %f\n", avgTat);
-      return 0;
+    int n;
+    printf("Enter total no of processes");
+    scanf("%d", &n);
+
+    int at[n], bt[n], rem_bt[n], sq = 0;
+    float tat, wt;
+    printf("enter arival time.....\n");
+    for (int i = 0; i < n; i++)
+    {
+        printf("at[%d] = ", i);
+        scanf("%d", &at[i]);
+    }
+
+    printf("enter burst time.....\n");
+    for (int i = 0; i < n; i++)
+    {
+        printf("bt[%d] = ", i);
+        scanf("%d", &bt[i]);
+        rem_bt[i] = bt[i];
+    }
+
+    int tc;
+    printf("enter time quantum = ");
+    scanf("%d", &tc);
+
+    int count = 0, temp, x = n, total = 0;
+    float awt, atat;
+
+    printf("at\t\tbt\t\ttat\t\twt\n");
+    for (int i = 0, total = 0; x != 0;)
+    {
+        if (rem_bt[i] <= tc && rem_bt[i] > 0)
+        {
+            total += rem_bt[i];
+            rem_bt[i] = 0;
+            count = 1;
+        }
+        else if (rem_bt[i] > 0)
+        {
+            rem_bt[i] -= tc;
+            total += tc;
+        }
+        if (rem_bt[i] == 0 && count == 1)
+        {
+            x--;
+            printf("%d\t\t%d\t\t%d\t\t%d\n", at[i], bt[i], total - at[i], total - at[i] - bt[i]);
+            tat += total - bt[i];
+            wt += total - at[i] - bt[i];
+            count = 0;
+        }
+        if (i == n - 1)
+        {
+            i = 0;
+        }
+        else if (at[i + 1] <= total)
+        {
+            i++;
+        }
+        else
+        {
+            i = 0;
+        }
+    }
+
+    printf("Average wt = %f\nAverage tat = %f\n", wt / n, tat / n);
+    return 0;
 }
